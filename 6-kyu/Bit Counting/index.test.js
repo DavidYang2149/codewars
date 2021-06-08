7ef3794211b18f1f082a0df3b1f3af4036474814
davidyang2149 <davidyang2149@gmail.com>
2021-06-08 23:01:18 +0900

    Add Bit Counting 6-kyu
    CodeWars
    - Add Bit Counting.

---
+++ 6-kyu/Bit Counting/index.test.js
@@ -0,0 +1,11 @@
+const countBits = require('.');
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    expect(countBits(0)).toEqual(0);
+    expect(countBits(4)).toEqual(1);
+    expect(countBits(7)).toEqual(3);
+    expect(countBits(9)).toEqual(2);
+    expect(countBits(10)).toEqual(2);
+  });
+});
