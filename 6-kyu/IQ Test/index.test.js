4d606c50711dfa0acb21be0a8eadc286623306f5
davidyang2149 <davidyang2149@gmail.com>
2021-06-17 20:54:31 +0900

    :white_check_mark: Update IQ Test (TestCode) 6-kyu
    CodeWars
    - Update IQ Test (TestCode).

--- 6-kyu/IQ Test/index.test.js
+++ 6-kyu/IQ Test/index.test.js
@@ -4,5 +4,6 @@ describe("Tests", () => {
   it("test", () => {
     expect(iqTest("2 4 7 8 10")).toEqual(3);
     expect(iqTest("1 2 2")).toEqual(1)
+    expect(iqTest("2 3 7")).toEqual(1)
   });
 });
