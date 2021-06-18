585b0c417df3d09712acd8910b4708337df48bc5
davidyang2149 <davidyang2149@gmail.com>
2021-06-18 21:52:42 +0900

    :art: Add Reversed Words 8-kyu
    CodeWars
    - Add Reversed Words.

---
+++ 8-kyu/Reversed Words/index.test.js
@@ -0,0 +1,11 @@
+const reverseWords = require('.');
+
+describe("reverseWords", function () {
+  it("should work for some examples", function () {
+    expect(reverseWords("hello world!")).toEqual("world! hello");
+    expect(reverseWords("yoda doesn't speak like this")).toEqual("this like speak doesn't yoda");
+    expect(reverseWords("foobar")).toEqual("foobar");
+    expect(reverseWords("kata editor")).toEqual("editor kata");
+    expect(reverseWords("row row row your boat")).toEqual("boat your row row row");
+  });
+});
